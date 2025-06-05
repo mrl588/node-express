2c07b026090d705547b55e17465ae22b0e7482a1
Michael Liu <liu.mike588@gmail.com>
2025-06-05 10:44:17 -0400

    first server example with http  Please enter the commit message for your changes. Lines starting

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,52 @@
-console.log('Express Tutorial')
+const http = require('http')
+const {readFileSync} = require('fs')
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req,res) => {
+    //console.log(req.method)
+    const url = req.url;
+    console.log(url)
+    //home
+    if (url ==='/'){
+        res.writeHead(200,{'content-type' : 'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    else if (url === '/about'){
+    //about page
+        res.writeHead(200,{'content-type' : 'text/html'})
+        res.write('<h1> about page </h1>')
+        res.end()
+    }
+    else if (url === '/styles.css'){
+    //styles
+        res.writeHead(200,{'content-type' : 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    }
+    else if (url === '/logo.svg'){
+    //logo
+        res.writeHead(200,{'content-type' : 'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    else if (url === '/browser-app.js'){
+    //logo
+        res.writeHead(200,{'content-type' : 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
+    //404
+    else{
+        res.writeHead(404, {'content-type' : 'text/html'})
+        res.write('<h1> Page not found </h1>')
+        res.end()
+    }
+})
+
+server.listen(3000)
