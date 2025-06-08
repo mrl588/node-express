e481ff5d92f00d2edb6f6db9d02cdc520f7159f7
Michael Liu <liu.mike588@gmail.com>
2025-06-08 08:36:19 -0400

    update day 7 of learning node and express , learned about the basics of middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,32 +1,24 @@
 const express = require('express')
 const app = express();
-const {products} = require('./data')
 
-app.get('/',(req,res) =>{
-    res.send('<h1> Home Page </h1> <a href="/api/products">products</a>')
+// req => middleware => res
+
+function logger(req,res,next) {
+    const method = req.method; // express  will pass it in itself
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method,url,time)
+    next() // pass it onto the next middleware
+    //res.send('testing')// will terminate cycle
+}
+
+app.get('/', logger , (req,res) => {
+    res.send('Home')
+})
+app.get('/about',logger,(req,res) => {
+    res.send('About')
 })
 
-app.get('/api/products', (req,res) =>{
-    const newProduct = products.map((product) => {
-        const {id,name,image} = product;
-        return {id,name,image}
-    })
-    res.json(newProduct)
-})
-app.get('/api/products/:productID', (req,res) =>{
-    //console.log(res)
-    //console.log(req.params)
-    const {productID} = req.params;
-
-    const singleProduct = products.find(
-        (product)=> product.id === Number(productID))
-    
-        if(!singleProduct){
-            return res.status(404).send('product doenst exist')
-        }
-    res.json(singleProduct)
-})
-
-app.listen(3000, ()=>{
-    console.log('server is listening')
+app.listen(3000, () => {
+    console.log('server is listening on port 3000')
 })
