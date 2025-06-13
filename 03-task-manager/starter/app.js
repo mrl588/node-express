35b9809af04f1e4fe288ccfc472fc14aabe9a544
Michael Liu <liu.mike588@gmail.com>
2025-06-13 10:39:03 -0400

    day 12 , started to learn mongo db and mongoose

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,11 @@
+require('./db/connect')
+require('dotenv').config()
 const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
-const port = 3000
+
 
 //midleware
 app.use(express.json())
@@ -20,4 +23,18 @@ app.use ('/api/v1/tasks', tasks)
 // app.patch('/api/v1/tasks/:id) - update a task
 // app.delete ('/api/v1/tasks/:id') - delete task
 
-app.listen(port , console.log(`server is listening on port ${port}...`))
+const port = 3000
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port , console.log(`server is listening on port ${port}...`))
+    }catch(error){
+        console.log(error)
+
+    }
+}
+
+start()
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
