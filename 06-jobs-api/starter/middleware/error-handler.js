75a5e87f1af807e1141ba6947ef2880a3d7f4663
Dark5888 <liu.mike588@gmail.com>
2025-07-21 19:17:00 -0400

    validation error

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -9,11 +9,16 @@ let customError = {
 
 }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+if (err.name === 'ValidationError') {
+  customError.msg = Object.values(err.errors)
+    .map(item => item.message)
+    .join(', ');
+  customError.statusCode = 400;
+}
+
+
   if(err.code && err.code === 11000){
-    customError.msg = `Duplicate value entered for ${err.keyValue} field, please choose another value`
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = 400 
   }
   //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
