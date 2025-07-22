f8e4fefba49686476d5acce5ab7b915f219bd90c
Michael Liu <liu.mike588@gmail.com>
2025-07-22 15:42:11 -0400

    finished job api

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -16,6 +16,11 @@ if (err.name === 'ValidationError') {
   customError.statusCode = 400;
 }
 
+if (err.name === "CastError"){
+  customError.msg = `No item found with id: ${err.value}`
+  customError.statusCode = 404
+} 
+
 
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
